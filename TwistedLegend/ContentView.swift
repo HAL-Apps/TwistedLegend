317aea1e2c6cee9db6498875e41b2d756aa7d778
EPA <emmett.paul@icloud.com>
2021-05-05 18:59:50 -0700

    Initial Commit

---
+++ TwistedLegend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwistedLegend
+//
+//  Created by Emmett Austin on 5/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwistedLegend/TwistedLegendApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwistedLegendApp.swift
+//  TwistedLegend
+//
+//  Created by Emmett Austin on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TwistedLegendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

14777a1b10210fa59b22fd596a4b383d3b48962e
EPA <emmett.paul@icloud.com>
2021-05-12 09:59:09 -0700

    adds rest of data

--- TwistedLegend/Stat.swift
+++ TwistedLegend/Stat.swift
@@ -8,10 +8,30 @@
 import Foundation
 
 struct Stat: Codable {
+  let strong: String
+  let `super`: String
+  let description: String
+  let species: String
+  let speed: String
+  let category: String
+  let defense: Int
+  let armor: Int
+  let criticalChance: Int
   let damage: Int
   let health: Int
   
+  
+  
   init() {
+    self.strong = ""
+    self.super = ""
+    self.description = ""
+    self.species = ""
+    self.speed = ""
+    self.category = ""
+    self.defense = 0
+    self.armor = 0
+    self.criticalChance = 0
     self.damage = 0
     self.health = 0
   }
--- TwistedLegend/StatView.swift
+++ TwistedLegend/StatView.swift
@@ -11,9 +11,22 @@ struct StatView: View {
   let data: StatViewModel
   
   var body: some View {
-    Text("\(data.name)")
-    Text("D = \(data.stats[0].damage)")
-    Text("H = \(data.stats[0].health)")
+    VStack {
+      Text("\(data.name)")
+      Text("S = \(data.stats[0].strong)")
+      Text("Super = \(data.stats[0].super)")
+      Text("D = \(data.stats[0].description)")
+      Text("S = \(data.stats[0].species)")
+      Text("S = \(data.stats[0].speed)")
+      Text("C = \(data.stats[0].category)")
+    }
+    VStack {
+      Text("D = \(data.stats[0].defense)")
+      Text("A = \(data.stats[0].armor)")
+      Text("CC = \(data.stats[0].criticalChance)")
+      Text("D = \(data.stats[0].damage)")
+      Text("H = \(data.stats[0].health)")
+    }
   }
 }
 

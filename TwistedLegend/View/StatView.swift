ecd3f4740d12c7b9a426b42c8a0a05d034afdb33
EPA <emmett.paul@icloud.com>
2021-05-16 18:46:17 -0700

    changes view

--- TwistedLegend/View/StatView.swift
+++ TwistedLegend/View/StatView.swift
@@ -15,29 +15,50 @@ struct StatView: View {
       
       VStack(spacing: 3) {
         Text("\(data.name)")
-//        if data.name == "Amakar" {
-//          Text("Boss = \(data.stats[0].boss ?? "ogo")")
-//        }
-        Text("lvl = \(data.stats[0].level)")
-        Text("S = \(data.stats[0].strong)")
+        if data.name == "Amakar" {
+          Text("Boss = \(data.stats[0].boss ?? "ogo")")
+        }
+        if data.name == "Goliath" {
+          Text("Boss = \(data.stats[0].boss ?? "ogo")")
+        }
+        if data.name == "Shadowbent" {
+          Text("Boss = \(data.stats[0].boss ?? "ogo")")
+        }
+        Text("level = \(data.stats[0].level)")
+          .padding(.vertical, 10)
+        Text("Strong = \(data.stats[0].strong)")
+          .padding(.vertical, 10)
         Text("Super = \(data.stats[0].super)")
-        Text("D = \(data.stats[0].description)")
-        Text("S = \(data.stats[0].species)")
-        Text("S = \(data.stats[0].speed)")
-        Text("C = \(data.stats[0].category)")
+          .padding(10)
+        Text("Description = \(data.stats[0].description)")
+          .padding(10)
+        Text("Species = \(data.stats[0].species)")
+          .padding(.vertical, 5)
+        Text("Speed = \(data.stats[0].speed)")
+          .padding(.vertical, 5)
       }
       VStack(spacing: 3) {
-        Text("D = \(data.stats[0].defense)")
-        Text("A = \(data.stats[0].armor)")
-        Text("CC = \(data.stats[0].criticalChance)")
-        Text("D = \(data.stats[0].damage)")
-        Text("H = \(data.stats[0].health)")
-        //        if data.name == "Stone keeper" {
-        //          Text("CL = \(data.stats[0].controlLevel ?? 1)")
-        //        }
-        Text("CR = Once every \(data.stats[0].chargeRate) minutes")
-        Text("D = \(data.stats[0].duration) seconds")
-        Text("SD = \(data.stats[0].superDamage)")
+        Text("Category = \(data.stats[0].category)")
+          .padding(.vertical, 5)
+        Text("Defense = \(data.stats[0].defense)")
+          .padding(.vertical, 5)
+        Text("Armor = \(data.stats[0].armor)")
+          .padding(.vertical, 5)
+        Text("Critical Chance = \(data.stats[0].criticalChance)")
+          .padding(.vertical, 5)
+        Text("Damage = \(data.stats[0].damage)")
+          .padding(.vertical, 5)
+        Text("Health = \(data.stats[0].health)")
+          .padding(.vertical, 5)
+        if data.name == "Stone keeper" {
+          Text("Control Level = \(data.stats[0].controlLevel ?? 1)")
+        }
+        Text("Charge Rate = Once every \(data.stats[0].chargeRate) minutes")
+          .padding(.vertical, 5)
+        Text("Duration = \(data.stats[0].duration) seconds")
+          .padding(.vertical, 5)
+        Text("Super Damage = \(data.stats[0].superDamage)")
+          .padding(.vertical, 5)
       }
     }
   }
